96fd1e1a692847f9cdc6562003f0bb03d7500b91
Pedro Aparecido Possari <pedropossari2@gmail.com>
2025-06-24 15:18:11 -0300

    feat: estrutura base

---
+++ src/main/java/Game/friends/GameFriends/GameFriendsApplication.java
@@ -0,0 +1,13 @@
+package Game.friends.GameFriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameFriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameFriendsApplication.class, args);
+	}
+
+}

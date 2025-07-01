a61504ab13a4ac7efa38abed0c71dce2c7949bb7
Pedro Aparecido Possari <pedropossari2@gmail.com>
2025-07-01 09:50:02 -0300

    test: teste unitario para as classes Entity de jogo

---
+++ src/test/java/Game/friends/GameFriends/UsuarioJogoEntityTest.java
@@ -0,0 +1,29 @@
+package Game.friends.GameFriends;
+import Game.friends.GameFriends.entity.JogoEntity;
+import Game.friends.GameFriends.entity.UsuarioEntity;
+import Game.friends.GameFriends.entity.UsuarioJogo.UsuarioJogoEntity;
+import Game.friends.GameFriends.entity.UsuarioJogo.UsuarioJogoId;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UsuarioJogoEntityTest {
+
+    @Test
+    void testAllArgsConstructor() {
+        UsuarioJogoId id = new UsuarioJogoId(1, 2);
+        UsuarioEntity usuario = new UsuarioEntity();
+        usuario.setEmail("exemplo@email.com");
+
+        JogoEntity jogo = new JogoEntity();
+        jogo.setTitulo("Halo");
+
+        UsuarioJogoEntity entity = new UsuarioJogoEntity(id, usuario, jogo, 8.5, false);
+
+        assertEquals(id, entity.getId());
+        assertEquals(usuario, entity.getUsuarios());
+        assertEquals(jogo, entity.getJogos());
+        assertEquals(8.5, entity.getRating());
+        assertFalse(entity.getFavorito());
+    }
+}
---
+++ src/test/java/Game/friends/GameFriends/UsuarioJogoIdTest.java
@@ -0,0 +1,44 @@
+package Game.friends.GameFriends;
+
+import Game.friends.GameFriends.entity.UsuarioJogo.UsuarioJogoId;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UsuarioJogoIdTest {
+
+    @Test
+    void testAllArgsConstructorAndEquality() {
+        UsuarioJogoId id1 = new UsuarioJogoId(1, 100);
+        UsuarioJogoId id2 = new UsuarioJogoId(1, 100);
+        UsuarioJogoId id3 = new UsuarioJogoId(2, 200);
+
+        assertEquals(id1, id2);
+        assertNotEquals(id1, id3);
+        assertEquals(id1.hashCode(), id2.hashCode());
+        assertNotEquals(id1.hashCode(), id3.hashCode());
+    }
+
+    @Test
+    void testNoArgsConstructor() {
+        UsuarioJogoId id = new UsuarioJogoId();
+        assertNotNull(id);
+    }
+
+    @Test
+    void testSetAndGetUsingReflection() throws Exception {
+        UsuarioJogoId id = new UsuarioJogoId();
+
+        var fieldUsuario = UsuarioJogoId.class.getDeclaredField("idUsuario");
+        var fieldJogo = UsuarioJogoId.class.getDeclaredField("idJogo");
+
+        fieldUsuario.setAccessible(true);
+        fieldJogo.setAccessible(true);
+
+        fieldUsuario.set(id, 10);
+        fieldJogo.set(id, 50);
+
+        assertEquals(10, fieldUsuario.get(id));
+        assertEquals(50, fieldJogo.get(id));
+    }
+}

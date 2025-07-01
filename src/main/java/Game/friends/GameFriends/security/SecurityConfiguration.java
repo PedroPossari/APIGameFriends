2cc3b218c370fa70424e1919c9eb75ea3eeb80bf
Marcelo Dalcin <marcelo_s.dalcin@hotmail.com>
2025-07-01 11:51:24 -0300

    fix-bug/403paraget

--- src/main/java/Game/friends/GameFriends/security/SecurityConfiguration.java
+++ src/main/java/Game/friends/GameFriends/security/SecurityConfiguration.java
@@ -33,8 +33,8 @@ public class SecurityConfiguration {
 
                         .antMatchers("/", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .antMatchers("/auth/**").permitAll()
-                        .antMatchers("/jogos/**").hasRole("ADMIN")
                         .antMatchers(HttpMethod.GET,"/jogos/**").hasAnyRole("ADMIN", "USUARIO")
+                        .antMatchers("/jogos/**").hasRole("ADMIN")
                         .antMatchers("/jogos/review").hasAnyRole("ADMIN", "USUARIO")
                         .antMatchers("/jogos/favoritos").hasAnyRole("ADMIN", "USUARIO")
 

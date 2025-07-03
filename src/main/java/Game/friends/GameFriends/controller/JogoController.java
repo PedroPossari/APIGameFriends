fcfdfd514b25a49bcab72529231bd87dc5502d2f
Pedro Aparecido Possari <pedropossari2@gmail.com>
2025-07-03 11:04:32 -0300

    fix: arrumando erro jogoController

--- src/main/java/Game/friends/GameFriends/controller/JogoController.java
+++ src/main/java/Game/friends/GameFriends/controller/JogoController.java
@@ -82,9 +82,10 @@ public class JogoController implements JogoControllerDoc {
     }
 
     @GetMapping("/review/{idJogo}")
-    public ResponseEntity<ReviewCheckDTO> isReviewed(@PathVariable("idJogo")Integer idJogo) throws RegraDeNegocioException{
+    public ResponseEntity<ReviewCheckDTO> isReviewed(@PathVariable("idJogo")Integer idJogo) throws RegraDeNegocioException {
         return new ResponseEntity<>(jogoService.isReviewed(idJogo), HttpStatus.OK);
-      
+    }
+
     @GetMapping("/usuario/reviews")
     public ResponseEntity<List<ReviewDTO>> findReviewsByUsuario() throws RegraDeNegocioException {
         return new ResponseEntity<>(jogoService.findReviewsByUsuario(), HttpStatus.OK);

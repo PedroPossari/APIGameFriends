b1cdbfd1f2838faed04b65996d9e605d491fc9a2
Pedro Aparecido Possari <pedropossari2@gmail.com>
2025-07-11 14:30:07 -0300

    fix: adicionando o endpoint rele-user para o controller usuario

--- src/main/java/Game/friends/GameFriends/controller/UsuarioController.java
+++ src/main/java/Game/friends/GameFriends/controller/UsuarioController.java
@@ -1,12 +1,16 @@
 package Game.friends.GameFriends.controller;
 
 import Game.friends.GameFriends.controller.doc.UsuarioControllerDoc;
+import Game.friends.GameFriends.dto.Jogos.UsuarioComAvaliacaoDTO;
 import Game.friends.GameFriends.dto.Usuario.UsuarioDTO;
 import Game.friends.GameFriends.dto.Usuario.UsuarioSearchDTO;
 import Game.friends.GameFriends.exception.RegraDeNegocioException;
 import Game.friends.GameFriends.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -37,5 +41,15 @@ public class UsuarioController implements UsuarioControllerDoc {
     @GetMapping("/search/{id}")
     public ResponseEntity<UsuarioDTO> findById(@PathVariable("id") Integer id) throws RegraDeNegocioException {
         return new ResponseEntity<>(usuarioService.findById(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/role-usuario")
+    public ResponseEntity<Page<UsuarioComAvaliacaoDTO>> listarUsuariosComRoleUsuario(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "15") int size
+    ) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<UsuarioComAvaliacaoDTO> usuarios = usuarioService.listarUsuariosComAvaliacaoPorCargo("ROLE_USUARIO", pageable);
+        return ResponseEntity.ok(usuarios);
     }
 }
